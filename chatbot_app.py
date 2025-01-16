067880a21dc68095f4fe3b64d7d60b17e05d7d9c
Aryakumar Jaiswal <86922289+Aryakumarjaiswal@users.noreply.github.com>
2025-01-16 14:56:46 +0530

    Repititiveness resolved

--- chatbot_app.py
+++ chatbot_app.py
@@ -30,7 +30,7 @@ def transfer_to_customer_service():
         print(message)
         return message
 
-GEMINI_API_KEY = "AIzaSyCdfguznhfW90qvKx-lTUus8_ODQsGM3nk"
+GEMINI_API_KEY = "AIzaSyCdM--------------------3nk"
 genai.configure(api_key=GEMINI_API_KEY)
 
 
